14d2269bd555df867c21e22a054104ef8cad088d
Yuvi Yash <yyuvi.official@gmail.com>
2026-04-25 13:26:03 +0530

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'test-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
